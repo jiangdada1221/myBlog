ea5945aa599b85520968a3f7047cb97be7ca7e6b
yuepengjiang <jiangdada12344321@163.com>
2019-08-09 22:31:39 +0800

    finish authentication

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 //jshint esversion:6
-
+require('dotenv').config();
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://Yuepeng:jyp267366@cluster0-fxj1l.mongodb.net/blogDB",{ useNewUrlParser: true });
+
+// mongoose.connect("mongodb+srv://Yuepeng:jyp267366@cluster0-fxj1l.mongodb.net/blogDB",{ useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/testUser",{ useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -15,12 +21,30 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(session({
+  secret:"jyp is cool",
+  resave:false,
+  saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-const composeSchema = {
+const composeSchema = new mongoose.Schema({
   title:String,
   body:String
-};
+});
 
+const userSchema = new mongoose.Schema({
+  username:String,
+  password:String
+});
+
+
+userSchema.plugin(passportLocalMongoose);
+const User = new mongoose.model("User",userSchema);
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 const Compose = new mongoose.model("Compose",composeSchema);
 
 app.get("/",function(req,res) {
@@ -38,7 +62,12 @@ app.get("/about",function(req,res){
 });
 
 app.get("/compose",function(req,res) {
-  res.render("compose");
+  if(req.isAuthenticated()){
+      res.render("compose");
+  }
+  else {
+    res.redirect("/login");
+  }
 });
 
 app.post("/compose",function(req,res) {
@@ -51,6 +80,32 @@ app.post("/compose",function(req,res) {
   });
   input.save();
   res.redirect("/");
+});
+app.post("/register",function(req,res){
+  User.register({username:req.body.username},req.body.password,function(err,user){
+    if(err) {console.log(err);res.send("error");}
+    else {
+      passport.authenticate("local")(req,res,function(){
+        res.send("success!");
+      });
+    }
+  });
+});
+
+app.post("/login",function(req,res){
+  const user = new User({
+    username:req.body.username,
+    password:req.body.password
+  });
+  console.log(user);
+  req.login(user,function(err){
+    if(err){console.log(err);}
+    else {
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/compose");
+      });
+    }
+  });
 });
 
 app.get("/posts/:name",function(req,res){
@@ -65,6 +120,12 @@ app.get("/posts/:name",function(req,res){
   //   }
   // });
 });
+app.get("/register",function(req,res){
+  res.render("register");
+});
+app.get("/login",function(req,res){
+  res.render("login");
+});
 
 
 
@@ -75,6 +136,7 @@ app.get("/posts/:name",function(req,res){
 
 
 
-app.listen(process.env.PORT, function() {
+
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
